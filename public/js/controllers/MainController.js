f81f0a4fcab6912dfba2d9bd138b7a143939d37b
Jon Church <me@jonchurch.com>
2016-07-13 17:15:35 -0400

    added a toggle button

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,17 +1,21 @@
-(function(){
-	angular.module('ngtodos') //getter
-			.controller('MainController', MainController);
+(function() {
+    angular.module('ngtodos') //getter
+        .controller('MainController', MainController);
 
-	MainController.$inject = ['$scope', 'TodoService'];
+    MainController.$inject = ['$scope', 'TodoService'];
 
-	function MainController($scope, TodoService) {
-		$scope.message = 'PizzaPasta';
-		var todos;
-		TodoService.readAll()
-					.then(function(){
-						todos = TodoService.todos;
-						console.log(todos);
-					});
-		
-	}
+    function MainController($scope, TodoService) {
+        $scope.message = 'PizzaPasta';
+        var todos;
+        TodoService.readAll()
+            .then(function() {
+                todos = TodoService.todos;
+                console.log(todos);
+            });
+            
+        $scope.visible = true;
+        $scope.toggle = function() {
+            $scope.visible = !$scope.visible;
+        };
+    }
 })();
