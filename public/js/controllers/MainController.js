ab4a81da2e9361add14c3fe3a0006fa8e1044976
Jon Church <me@jonchurch.com>
2016-07-13 19:47:33 -0400

    todo view template, featuring for each and sprinkles wrapped up in a jumbotron

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -21,7 +21,6 @@
         function createTodo(description){
          TodoService.create(description)
                     .then(function(){
-                        $scope.todos = TodoService.todos;
                         $scope.description = '';
                         getTodos();
                     });
